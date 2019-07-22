9bcc2c1d7c79933e32d98943c37bf305d6e04452
Jacob Boyd <jacobboyd14012@gmail.com>
2019-07-21 23:05:56 -0400

    Added edit and create react components

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -7,7 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors);  // allows cross origin resources
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));  // allows cross origin resources
 app.use(express.json()); //allow to parse json
 
 //connection to database
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercies.component";
+import EditExercise from "./components/edit-exercises.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 import './App.css';
@@ -11,12 +11,14 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/usser" component={CreateUser} />
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
